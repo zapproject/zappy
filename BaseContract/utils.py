d01b5dffaf10fae9ba5ff7e3ab063c9a319a43af
Andrew <andrewfleming@Andrews-MacBook-Pro.local>
2021-02-15 16:55:13 -0500

    Added unit tests for utils, condensed utils functions and included extra args for testing

--- BaseContract/utils.py
+++ BaseContract/utils.py
@@ -1,29 +1,37 @@
 import json
 import os
 
-
 # These functions replace the monorepo's artifactDir and negates the need for each 
 # artifact's location to be hardcoded in a seperate module.
-#
-# Helper function for load_abi and load_address.
-def load_json(name: str) -> str:
-    # Adjust path below to fetch Artifacts/<name>.json.
-    path = #f"{os.path.dirname(os.path.dirname(__file__))}/Artifacts/"
-    with open(os.path.abspath(path + f"{name}.json")) as f:
-        json_file = json.load(f)
-    return json_file
 
 
-def load_abi(name: str) -> dict:
-    obj = load_json(name)
-    abi = obj['abi']
-    return abi
-
-def load_address(name: str, netId: int or str) -> dict:
+# Fetches ABI and is a helper function for load_address.
+#   @params: (name) is the artifact name
+#            (file_path) is strictly for testing
+def load_abi(name: str, file_path = None) -> dict:
+# Adjust path below (if changed) to fetch Artifacts/<name>.json.
+    if file_path is not None:
+        path = file_path
+    else:
+        path = f"{os.path.dirname(os.path.dirname(__file__))}/Artifacts/"
     try:
-        a_dict = load_json(name)
-        addr = a_dict['networks']
-        network_address = addr[netId]['address']
+        with open(os.path.abspath(path + f"{name}.json")) as f:
+            abi_file = json.load(f)
+        return abi_file
+    except Exception as e:
+        print(e)
+
+#   @params (name) is the name of the artifact
+#           (netId) is the Ethereum network
+#           (abi_file) is used for test
+def load_address(name: str, netId: int or str, abi_file = None) -> str:
+    try:
+        if abi_file is not None:
+            a_dict = abi_file
+        else:
+            a_dict = load_abi(name)
+        network_key = a_dict['networks']
+        network_address = network_key[netId]['address']
         return network_address
     except Exception as e:
         print('Error with: ' + str(e))
