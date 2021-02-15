8951b10f0326cd5609b14e2eefb7589140b871e8
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-02-15 18:37:46 -0400

    fixed syntax errors

--- BaseContract/test/test_base_contract.py
+++ BaseContract/test/test_base_contract.py
@@ -1,8 +1,7 @@
+from unittest import TestCase
 from unittest.mock import Mock, MagicMock, patch
 import base_contract
 import utils
 
-class test_base_contract(unittest.TestCase):
-    
-
-
+class test_base_contract(TestCase):
+    pass
