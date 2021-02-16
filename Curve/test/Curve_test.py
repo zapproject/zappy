bd8b6391c3e733f616c2c88729db274e40d74d77
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-02-16 05:41:04 -0400

    Added more tests for curve validation;
    Curve/test/Curve_test - added fixture to init more curves
                          - added expected exception messages for each case
    
    Curve/types.py - fixed spelling for CurveTerm interface

--- Curve/test/Curve_test.py
+++ Curve/test/Curve_test.py
@@ -1,7 +1,25 @@
-from pytest import raises
+from pytest import raises, fixture
 from Curve.curve import Curve
 
 
-def test_init():
-    with raises(Exception):
-        Curve([-1, 0, 0, 3, 1e10]).checkValidity()
+@fixture
+def curves():
+    """Curve object values validity test
+
+        The second parameter in each return element is the expected
+        Exception class message.
+    """
+    return [([-1, 0, 0, 3, 1e10], "Invalid curve length"),
+            ([4, 0, 0, 3, 1e10], "Piece is out of bounds"),
+            ([3, 0, 0, 3, 1e10, 3, 0, 0, 4, 1e10], "Piece domains are overlapping"),
+            ([3, 0, 0, 3, 1e10, 3, 0, 0, 4, 1e9], "Piece domains are overlapping"),
+            ([3, 0, 0, 3, 1e10, 3, 0, 0, 4, -1], "Piece domains are overlapping")]
+
+
+def test_init(curves):
+    """
+        Tests the initialization and validation of curves
+    """
+    for curve in curves:
+        with raises(Exception, match=curve[1]):
+            Curve(curve[0])
--- Curve/types.py
+++ Curve/types.py
@@ -3,7 +3,7 @@ from typing import NewType, TypedDict
 CurveType = NewType("CurveType", list)
 
 
-class CureveTerm(TypedDict):
+class CurveTerm(TypedDict):
     fn: int or float
     power: int or float
     coef: int or float
