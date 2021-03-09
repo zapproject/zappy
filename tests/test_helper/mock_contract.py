0a4dfae64b4dfe3c464dd4d2c4cdb8bbe37f939e
Jayon Kim <jpkim921@gmail.com>
2021-03-09 15:19:11 -0500

    add mock_contract

---
+++ tests/test_helper/mock_contract.py
@@ -0,0 +1,72 @@
+from unittest import mock
+from web3 import Web3
+from unittest.mock import MagicMock
+import json
+import pprint
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath("./__file__")))
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+class MockContract():
+
+    def __init__(self, contract_name:str, node_url: str = "http://127.0.0.1:8545"):
+        self.node_url = node_url
+        # self.network_id = network_id
+        self.web3 = Web3(Web3.HTTPProvider(node_url))
+        self.contract = self.connectToContract(contract_name)
+
+    def getArtifact(self, contract_name: str):
+        base_path = os.path.dirname(os.path.abspath("./__file__"))
+        artifacts_directory = "src/Artifacts/contracts"
+        artifact_path = os.path.join(
+            base_path, artifacts_directory, f"{contract_name.capitalize()}.json")
+        with open(artifact_path) as f:
+            artifact = json.load(f)
+            return artifact
+
+    # def pprint_dir(self):
+    #     pp.pprint(dir(self.web3))
+
+    def connectToContract(self, contract_name):
+        artifacts = self.getArtifact(contract_name)
+        contract_abi = artifacts['abi']
+        contract_address = artifacts['networks'][str(
+            self.web3.eth.chain_id)]['address']
+        contract = self.web3.eth.contract(
+            address=contract_address, abi=contract_abi)
+        return contract
+
+    def list_of_functions(self):
+        return dir(self.contract.functions)
+
+    def mock_func_rv(self, function_name, rv):
+        try:
+            func = self.contract.get_function_by_name(function_name)
+            # func = getattr(self.contract.functions, function_name)
+            # print(dir(func))
+            # print(func.fn_name)
+            if func:
+                mock_contract = MagicMock()
+                mock_contract.functions.function_name = MagicMock(return_value=rv)
+                # mock_contract.functions.return_value = rv
+                # print(mock_contract)
+                return mock_contract
+                # print(self.contract.functions.func.fn_name)
+            #     self.contract.functions.func.return_value = rv
+        except Exception as e:
+            print(e)
+    
+
+
+registry = MockContract('registry')
+registry.mock_func_rv('initiateProvider', 'woof')
+mock_registry = registry.mock_func_rv('initiateProvider', 'woof')
+print(mock_registry.functions.initiateProvider())
+
+# pp.pprint(dir(registry.contract.functions.initiateProvider))
+# print(registry.mock_func_rv('initiateProvider', 'woof'))
+# pp.pprint(dir(registry.contract.functions))
