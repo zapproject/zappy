773d4d3420ab4ab4031516b52541175aa3b280d5
Alex D <taejoonn@gmail.com>
2022-01-25 18:46:01 -0500

    Working mint transactions

--- src/nft/zapMedia_regular.py
+++ src/nft/zapMedia_regular.py
@@ -2,8 +2,8 @@ from base_contract import BaseContract
 
 class ZapMedia(BaseContract):
 
-    def __init__(self, signer_or_wallet, chainId):
-        super().__init__(signer_or_wallet, chainId)
+    def __init__(self, chainId):
+        super().__init__(chainId)
 
         try:
             self.connect_to_contract("ZapMedia")            
@@ -19,8 +19,7 @@ class ZapMedia(BaseContract):
         return func().call()
 
 
-params = ["signer", '31337']
-
+params = ['31337']
 # connecting to Zap Media PROXY instance
 zap_media = ZapMedia(*params)
 print(zap_media.chainId)
@@ -30,3 +29,34 @@ print(zap_media.w3)
 print(dir(zap_media.contract.functions))
 print(zap_media.contract.functions.getOwner().call())
 print(zap_media.call_function("getOwner"))
+
+nonce = zap_media.w3.eth.getTransactionCount("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+
+private_key = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+
+tx = zap_media.contract.functions.mint(
+        {
+            "tokenURI": "token-uri",
+            "metadataURI": "metadata-uri",
+            "contentHash": b'1',
+            "metadataHash": b'2',
+        },
+        {
+            "creator": {"value": 95000000000000000000},
+            "owner": {"value": 0},
+            "collaborators": [],
+            "collabShares": []
+        }
+    ).buildTransaction({
+        'chainId': 31337,
+        'gas': 1400000,
+        'gasPrice': zap_media.w3.toWei('50', 'gwei'),
+        'nonce': nonce,
+    })
+
+signed_txn = zap_media.w3.eth.account.signTransaction(tx, private_key)
+
+result = zap_media.w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+tx_receipt = zap_media.w3.eth.getTransactionReceipt(result)
+print(tx_receipt)
