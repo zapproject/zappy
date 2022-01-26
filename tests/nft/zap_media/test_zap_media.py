40994d9450ce3fa62d09ed43830cc3c406a88372
Alex D <taejoonn@gmail.com>
2022-01-26 16:57:11 -0500

    Clean up test file

--- tests/nft/zap_media/test_zap_media.py
+++ tests/nft/zap_media/test_zap_media.py
@@ -30,11 +30,6 @@ def test_address_matches_deployed_address():
     deployed_zap_media_contract_hardhat_address = "0x3Ca8f9C04c7e3E1624Ac2008F92f6F366A869444"
     assert zap_media.address == deployed_zap_media_contract_hardhat_address
 
-
-
-
-
-
 def test_media_mint():
     # mint token
     media_data = zap_media.makeMediaData("token-uri", "metadata-uri", b"content-hash2", b"metadata-hash")
@@ -50,48 +45,6 @@ def test_media_mint():
 
     assert zap_media.tokenByIndex(0) == 0
 
+
+
 test_media_mint()
-
-
-
-# ====================================
-# ====================================
-# LEGACY CODE BELOW - prob can delete
-# ====================================
-# ====================================
-# from unittest.mock import MagicMock, patch
-
-# from src.BaseContract.base_contract import BaseContract
-
-
-# MockContract = MagicMock(address="0x000000000000000000", name="MOCKCONTRACT")
-# @patch('src.BaseContract.base_contract.Web3', autospec=True)
-# def test_instance(mock_Web3):
-#     # mock w3.eth.contract to return a mocked contract
-#     w3 = MagicMock()
-    
-#     # set return value of Web3(HTTPProvider) to mocked w3 above
-#     mock_Web3.HTTPProvider.return_value = w3
-#     w3.eth.contract.return_value = MockContract
-    
-#     instance = BaseContract("ARBITER", mock_Web3)
-#     assert instance
-
-#     # w3.eth.contract = MagicMock(address="WOOFWOOF")
-    
-#     # print(dir(w3))
-#     # # contract = 
-#     # # print(w3.eth.contract())
-
-#     # contract = w3.eth.contract()
-#     # print(contract.address)
-#     # print(contract)
-    
-
-# # test_instance()
-
-    
-    
-    
-#     instance = BaseContract('ARBITER', mock_Web3)
-#     assert instance
