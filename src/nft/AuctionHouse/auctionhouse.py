2258cd37f5e523bd02ab7167014e4522c733c15c
Jayon Kim <jpkim921@gmail.com>
2022-02-03 00:12:33 -0500

    add auctions getter and have it return a more friendly AuctionInfo class.

--- src/nft/AuctionHouse/auctionhouse.py
+++ src/nft/AuctionHouse/auctionhouse.py
@@ -1,4 +1,23 @@
 from src.nft.base_contract import BaseContract
+from src.nft.AuctionHouse.auctionInfo import AuctionInfo
+from typing import TypedDict
+
+class TokenDetails(TypedDict, total=False):
+    tokenId: int
+    mediaContract: str
+
+class Auction(TypedDict, total=False):
+    token: TokenDetails
+    approved: bool
+    amount: int
+    duration: int
+    firstBidTime: int
+    reservePrice: int
+    curatorFeePercentage: int 
+    tokenOwner: str
+    bidder: str
+    curator: str
+    auctionCurrency: str
 
 
 class AuctionHouse(BaseContract):
@@ -10,14 +29,15 @@ class AuctionHouse(BaseContract):
         except Exception as e:
             print(e)
         
-    def auctions(self, ):
-        return self.contract.functions.auctions()
-            
+    def auctions(self, auctionId: int) -> AuctionInfo:
+        response = self.contract.functions.auctions(auctionId).call()
+        return AuctionInfo(response)
+
     def cancelAuction(self, auctionId):
         return self.contract.functions.cancelAuction(auctionId)
             
     def createAuction(self, tokenId, mediaContract, duration, reservePrice, curator, curatorFeePercentage, auctionCurrency):
-        return self.contract.functions.createAuction(tokenId, mediaContract, duration, reservePrice, curator, curatorFeePercentage, auctionCurrency)
+        return self.sendTransaction(self.contract.functions.createAuction(tokenId, mediaContract, duration, reservePrice, curator, curatorFeePercentage, auctionCurrency))
             
     def createBid(self, auctionId, amount, mediaContract):
         return self.contract.functions.createBid(auctionId, amount, mediaContract)
