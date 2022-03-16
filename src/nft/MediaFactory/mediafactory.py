84c5ae3bb74a2e02d997f417165ce19056836d5d
Jayon Kim <jpkim921@gmail.com>
2022-03-16 18:00:25 -0400

    add some info to deploy_media function

--- src/nft/MediaFactory/mediafactory.py
+++ src/nft/MediaFactory/mediafactory.py
@@ -15,15 +15,29 @@ class MediaFactory(BaseContract):
     # def deploy_media(self, name: str, symbol: str, marketContractAddr: str, permissive: bool, _collectionMetadata: str):
     #     return self.send_transaction(self.contract.functions.deployMedia(name, symbol, marketContractAddr, permissive, _collectionMetadata))
 
+
     def deploy_media(self, name: str, symbol: str, marketContractAddr: str, permissive: bool, _collectionMetadata: str):
+        """
+        Deploys a custom Zap Media contract for user/users to mint as well as interact with the Zap NFT Marketplace.
+
+        :param str name: name of collection
+        :param str symbol: symbol of collection
+        :param str marketContractAddr: associated zap market contract address
+        :param bool permissive: permit other users to mint from this collection
+        :param str _collectionMetadata: URI of collection metadata
+
+        :example:
+            args = ['Collection Name', 'CSYM', zap_market.address, True, "https://ExampleCollectionURI.com"]
+
+        >>> zap_media = media_factory.deploy_media(*args)
         
-        # tx_deploy_media = self.send_transaction(self.contract.functions.deployMedia(name, symbol, marketContractAddr, permissive, _collectionMetadata))
-        self.send_transaction(self.contract.functions.deployMedia(name, symbol, marketContractAddr, permissive, _collectionMetadata))
-        # receipt = self.w3.eth.wait_for_transaction_receipt(tx_deploy_media, 180)
+        """
+        tx_deploy_media = self.send_transaction(self.contract.functions.deployMedia(name, symbol, marketContractAddr, permissive, _collectionMetadata))
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_deploy_media, 180)
         logs = self.contract.events.MediaDeployed.getLogs()
         event = logs[0]
         deployed_media_address = event.args.mediaContract
-        print("deployed_media_address: ", deployed_media_address)
-        return deployed_media_address
+        return (receipt, deployed_media_address)
+
 
             
